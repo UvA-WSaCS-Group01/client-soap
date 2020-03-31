d9c3e73911cfdb9ac49316cf46f5a00d8da64a27
Leonard Herold <git@hdld.me>
2020-03-31 22:01:05 +0200

    add status text on fail

--- CalculatorWS_Client_Application/src/main/java/com/wsacs/wsacs/soap/client/calculatorws_client_application/Main.java
+++ CalculatorWS_Client_Application/src/main/java/com/wsacs/wsacs/soap/client/calculatorws_client_application/Main.java
@@ -28,6 +28,8 @@ public class Main {
             System.out.println("DIV: " +port.div(x, y));
         } catch(Exception e){
             // handle exception
+            System.out.println("Failed to call service");
+            System.out.println(e);
         }
 
     }
