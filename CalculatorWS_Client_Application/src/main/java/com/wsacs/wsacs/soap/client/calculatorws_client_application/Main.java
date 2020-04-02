e0b08cd06f394b11a59d422e20601e50d535037d
Stijn Meijerink <stijn.meijerink@gmail.com>
2020-04-02 11:25:05 +0200

    Real numbers and change of interface

--- CalculatorWS_Client_Application/src/main/java/com/wsacs/wsacs/soap/client/calculatorws_client_application/Main.java
+++ CalculatorWS_Client_Application/src/main/java/com/wsacs/wsacs/soap/client/calculatorws_client_application/Main.java
@@ -11,8 +11,8 @@ package com.wsacs.wsacs.soap.client.calculatorws_client_application;
  */
 public class Main {
     public static void main(String args[]){
-        int x = 10;
-        int y = 2;
+        double x = 10;
+        double y = 3;
         
         System.out.println("Welcome to the JAX-WS calculator!");
         System.out.println("Chosen input is x " +x +", y " +y);
@@ -22,10 +22,10 @@ public class Main {
             com.wsacs.wsacs.soap.web.calculator.CalculatorService_Service service = new com.wsacs.wsacs.soap.web.calculator.CalculatorService_Service();
             com.wsacs.wsacs.soap.web.calculator.CalculatorService port = service.getCalculatorServicePort();
             
-            System.out.println("ADD: " +port.add(x, y));
-            System.out.println("MIN: " +port.min(x, y));
-            System.out.println("MUL: " +port.mul(x, y));
-            System.out.println("DIV: " +port.div(x, y));
+            System.out.println("Addition: " +port.add(x, y));
+            System.out.println("Substraction: " +port.sub(x, y));
+            System.out.println("Multiplication: " +port.mul(x, y));
+            System.out.println("Division: " +port.div(x, y));
         } catch(Exception e){
             // handle exception
             System.out.println("Failed to call service");
